c0b07cb5fd6aa9549d721c8ecb0bff26bac1d450
ks srinath <kssrinath@kss-MacBook-Pro.local>
2024-09-05 16:40:40 +0530

    Initial Commit

---
+++ ascii.py
@@ -0,0 +1,140 @@
+import json
+import requests
+import streamlit as st, pandas as pf
+import plotly.express as px 
+from PIL import Image
+
+# Page title
+st.title('ASCII Image generator')
+
+from io import StringIO
+
+### TO UPLOAD THE IMAGE ###
+uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg"])
+
+### TO CONVERT THE IMAGE TO ASCII in .txt format ###
+# ascii characters used to build the output text
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+# resize image according to a new width
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height/width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return(resized_image)
+
+# convert each pixel to grayscale
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+    
+# convert pixels to a string of ascii characters
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)    
+
+def main(new_width=100):
+    # attempt to open image from user-input
+    path = uploaded_file
+    image = Image.open(path)
+  
+    # convert image to ascii    
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+    
+    # format
+    pixel_count = len(new_image_data)  
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
+    
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+
+### CONVERT ascii_image.txt to png format ###
+from math import ceil
+
+from PIL import (
+    Image,
+    ImageFont,
+    ImageDraw,
+)
+
+PIL_GRAYSCALE = 'L'
+PIL_WIDTH_INDEX = 0
+PIL_HEIGHT_INDEX = 1
+COMMON_MONO_FONT_FILENAMES = [
+    'DejaVuSansMono.ttf',  # Linux
+    'Consolas Mono.ttf',   # MacOS, I think
+    'Consola.ttf',         # Windows, I think
+]
+
+
+def convert():
+    image = textfile_to_image('ascii_image.txt')
+    image.save('content.png')
+
+
+def textfile_to_image(textfile_path):
+    """Convert text file to a grayscale image.
+
+    arguments:
+    textfile_path - the content of this file will be converted to an image
+    font_path - path to a font file (for example impact.ttf)
+    """
+    # parse the file into lines stripped of whitespace on the right side
+    with open(textfile_path) as f:
+        lines = tuple(line.rstrip() for line in f.readlines())
+
+    # choose a font (you can see more detail in the linked library on github)
+    font = None
+    large_font = 20  # get better resolution with larger size
+    for font_filename in COMMON_MONO_FONT_FILENAMES:
+        try:
+            font = ImageFont.truetype(font_filename, size=large_font)
+            print(f'Using font "{font_filename}".')
+            break
+        except IOError:
+            print(f'Could not load font "{font_filename}".')
+    if font is None:
+        font = ImageFont.load_default()
+        print('Using default font.')
+
+    # make a sufficiently sized background image based on the combination of font and lines
+    font_points_to_pixels = lambda pt: round(pt * 96.0 / 72)
+    margin_pixels = 20
+
+    # height of the background image
+    tallest_line = max(lines, key=lambda line: font.getsize(line)[PIL_HEIGHT_INDEX])
+    max_line_height = font_points_to_pixels(font.getsize(tallest_line)[PIL_HEIGHT_INDEX])
+    realistic_line_height = max_line_height * 0.8  # apparently it measures a lot of space above visible content
+    image_height = int(ceil(realistic_line_height * len(lines) + 2 * margin_pixels))
+
+    # width of the background image
+    widest_line = max(lines, key=lambda s: font.getsize(s)[PIL_WIDTH_INDEX])
+    max_line_width = font_points_to_pixels(font.getsize(widest_line)[PIL_WIDTH_INDEX])
+    image_width = int(ceil(max_line_width + (2 * margin_pixels)))
+
+    # draw the background
+    background_color = 255  # white
+    image = Image.new(PIL_GRAYSCALE, (image_width, image_height), color=background_color)
+    draw = ImageDraw.Draw(image)
+
+    # draw each line of text
+    font_color = 0  # black
+    horizontal_position = margin_pixels
+    for i, line in enumerate(lines):
+        vertical_position = int(round(margin_pixels + (i * realistic_line_height)))
+        draw.text((horizontal_position, vertical_position), line, fill=font_color, font=font)
+
+    return image
+
+
+if __name__ == '__main__':
+    convert()
+
+# run program
+main()
+convert()
+
+st.image("content.png", caption="YAHHOOOO")
